1f6b38b7b62c1650f780384b12a74554354c253c
KHELIFA Yassine <prostam27@gmail.com>
2021-10-17 12:08:32 +0200

    mapGetters & mapActions Helpers

--- src/components/FirstExample.vue
+++ src/components/FirstExample.vue
@@ -2,20 +2,29 @@
     <div>
         <h1>Vue vueX</h1>
         <h5>{{ counter }}</h5>
-        <button @click="inc">First Compo</button>
+        <button @click="inc(2)">First Compo</button>
+        <h2>{{ name}}</h2>
     </div>
 </template>
 <script>
+    import { mapGetters, mapActions } from 'vuex'
 export default {
     computed: {
-        counter(){
-            return this.$store.getters.getCounterNormalise
-        }
+        //counter(){
+         //   return this.$store.getters.getCounterNormalise
+        //}
+        ...mapGetters({
+            'counter' : 'getCounterNormalise',
+            'name': 'getName'
+        })
     },
     methods: {
-        inc(){
-            this.$store.dispatch('actionIncCounter',3)
-        }
+       // inc(){
+        //    this.$store.dispatch('actionIncCounter',3)
+        //}
+        ...mapActions({
+            'inc': 'actionIncCounter'
+        })
     },
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -14,6 +14,9 @@ export default createStore({
     getCounter(state){
       return state.count * 2;
     },
+    getName(state){
+      return state.name.toUpperCase();
+    },
     getCounterNormalise(_, getters){
       const counter =  getters.getCounter;
 
